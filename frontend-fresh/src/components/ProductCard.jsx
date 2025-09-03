8434ee9d0301d80d9f315f75b09db13b5c26ea62
Sindhu Uday Naik <sindhuudaynaik@gmail.com>
2025-09-04 00:21:16 +0530

    Updated ProductCard.jsx

--- frontend-fresh/src/components/ProductCard.jsx
+++ frontend-fresh/src/components/ProductCard.jsx
@@ -147,7 +147,7 @@ export default function ProductCard({ product }) {
           className="object-contain w-full h-48 object-cover rounded-md"
           style={{ width: "200px", height: "400px", objectFit: "cover" }}
         />
-        <h2 className="mt-2 text-white text-center text-lg font-semibold truncate">
+        <h2 className="mt-2 text-white text-center text-lg font-semibold truncate break-words line-clamp-2">
           {product.title}
         </h2>
         <p className="text-white-700 text-center font-bold text-base">
